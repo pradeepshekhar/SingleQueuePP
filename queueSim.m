3b3ee9d75eab4609b9a15ce3eef0021419fd792c
Daniel Lazar <daniellazar@169-231-133-126.wireless.ucsb.edu>
2017-06-01 18:20:37 -0700

    updated queue sim

--- queueSim.m
+++ queueSim.m
@@ -1,6 +1,7 @@
 % Single lot of size c with two types of vehicles
 % All prices are in $/hr
 % All notations according to "Queuing framework report"
+clear all;clc;
 
 %total no. of parking spots
 c=30;
@@ -31,14 +32,10 @@ mu_tilda = (mu_1*mu_2)/(gamma*mu_2 + (1-gamma)*mu_1);
 %Traffic intensity
 rho = lambda/(c*mu);
 
-% parking prices (Will be changed based on balking lavel later)
-P_1 = 0;
-P_2 = 0; 
-
 % Waiting cost
-P_w = 30; %48 used by ratliff et al
+P_w = 75; %48 used by ratliff et al
 
-% Reward for parking: single type, Type1 and Type2
+% Reward for parking for Type1 and Type2
 R = 75;
 R_1 = 75; R_2 = 75;
 %R_1 = R*lambda*(mu_2)/(lambda_1*mu_2+lambda_2*mu_1); R_2 = R*lambda*(mu_1)/(lambda_1*mu_2+lambda_2*mu_1); 
@@ -57,7 +54,7 @@ users = [type_1, type_2];
 
 % Note - P, P_1 and P_2 are changed dynamically to get the required balking
 % level. So, why create a structure with them? 
-lot = struct('num_spots', c, 'parking_cost_1', P_1, 'parking_cost_2', P_2);
+lot = struct('num_spots', c, 'parking_cost_1', 0, 'parking_cost_2', 0);
 
 % probability of acceptance of cars of types 1 and 2 
 % assigning some initial guess of the correct values
@@ -65,9 +62,6 @@ kappa = 0;
 zeta = 0.8; 
 
 % desired balking levels of vehicles
-n1=35;
-n2=60;
-
 n1_start = 30;
 n2_start = 30;
 n1_range = 30;
@@ -77,6 +71,15 @@ n2_range = 30;
 utility_total = zeros(n1_range+1,n2_range+1);
 utility_1 = zeros(n1_range+1,n2_range+1);
 utility_2 = zeros(n1_range+1,n2_range+1);
+
+gamma_grid = zeros(n1_range+1, n2_range+1);
+kappa_grid = zeros(n1_range+1, n2_range+1);
+zeta_grid = zeros(n1_range+1, n2_range+1);
+mutilde_grid = zeros(n1_range+1, n2_range+1);
+p_k_n_12_grid = zeros(n1_range+1, n2_range+1, max(n1_range, n2_range)+1);
+
+p1_grid = zeros(n1_range+1, n2_range+1);
+p2_grid = zeros(n1_range+1, n2_range+1);
 
 %Cycling through different pairs of (n1,n2) and finding utility
 for n1=n1_start:n1_start+n1_range
@@ -97,21 +100,59 @@ for n1=n1_start:n1_start+n1_range
         utility_1(x,y) = 0;
         mu_tilda = (mu_1*mu_2)/(gamma*mu_2 + (1-gamma)*mu_1);
         p_k_n_12 = findp_k_n_12(n1, n2, mu_tilda, lambda_1, lambda_2, c);
-        for i=1:n1
-            utility_total(x,y)=utility_total(x,y)+gamma*(lambda_1)*(p_k_n_12(i))*...
-                (findalpha(i-1, users(type_1_idx), lot, mu_tilda)); 
-        end
-        utility_1(x,y) = utility_total(x,y)/gamma;
         
-        for i=1:n2
-            utility_total(x,y)=utility_total(x,y)+(1-gamma)*(lambda_2)*(p_k_n_12(i))*...
-                (findalpha(i-1, users(type_2_idx), lot, mu_tilda));
+        
+        % Keep track of these levels
+        gamma_grid(x,y) = gamma;
+        kappa_grid(x,y) = kappa;
+        zeta_grid(x,y) = zeta;
+        mutilde_grid(x,y) = mu_tilda;
+        
+        for i = 1:n1
+            alpha_1(i) = findalpha(i-1, users(type_1_idx), lot, mu_tilda);
         end
-        utility_2(x,y) = (utility_total(x,y)-utility_1(x,y))/(1-gamma);
+        
+        % Calculate utility from type 1 and type 2 separately
+        for i = 1:n1
+            utility_1(x,y) = utility_1(x,y)+ p_k_n_12(i) * ...
+                findalpha(i-1, users(type_1_idx), lot, mu_tilda);
+        end
+%         utility_1(x,y) = utility_1(x,y) * gamma * lambda_1;
+         utility_1(x,y) = utility_1(x,y) * lambda_1;
+        
 
+        for i = 1:n2
+            utility_2(x,y) = utility_2(x,y) +  p_k_n_12(i) * ...
+                findalpha(i-1, users(type_2_idx), lot, mu_tilda);
+        end
+        %utility_2(x,y) = utility_2(x,y) * (1 - gamma) * lambda_2;
+        utility_2(x,y) = utility_2(x,y) * lambda_2;
+        
+        utility_total(x,y) = utility_1(x,y) + utility_2(x,y);
+        
+        [P1, P2] = findprices(n1, n2, c, R_1, R_2, P_w, W, mu_tilda, mu_1, mu_2);
+        p1_grid(x,y) = P1;
+        p2_grid(x,y) = P2;
+        
+        % Calculate utility together
+%         for i=1:n1
+%             utility_total(x,y)=utility_total(x,y)+gamma*(lambda_1)*(p_k_n_12(i))*...
+%                 (findalpha(i-1, users(type_1_idx), lot, mu_tilda)); 
+%         end
+%         utility_1(x,y) = utility_total(x,y)/gamma;
+%         
+%         for i=1:n2
+%             utility_total(x,y)=utility_total(x,y)+(1-gamma)*(lambda_2)*(p_k_n_12(i))*...
+%                 (findalpha(i-1, users(type_2_idx), lot, mu_tilda));
+%         end
+%         utility_2(x,y) = (utility_total(x,y)-utility_1(x,y))/(1-gamma);
         gamma=0;
+
     end
 end
+
+%%
+
 [U_total_max] = max(utility_total(:));
 [row, col] = find(utility_total == U_total_max);
 U_1_max = utility_1(row,col);
@@ -120,7 +161,31 @@ n1_max = n1_start+row-1;
 n2_max = n2_start+col-1;
 figure(1);
 mesh(utility_total);
+xlabel('type 2 balking rate');
+ylabel('type 1 balking rate');
+zlabel('utility');
 %{
 figure(2);
 plot(p_k_n_12);
 %}
+
+[p1_grid(row,col), p2_grid(row,col)]
+pos_prices = (p1_grid > 0) & (p2_grid > 0);
+feas_util = utility_total .* pos_prices; 
+
+figure(2); clf;
+subplot(2,1,1);
+mesh(utility_total);
+xlabel('type 2 balking rate');
+ylabel('type 1 balking rate');
+zlabel('utility');
+title('Utility for all balking rates');
+
+subplot(2,1,2);
+mesh(feas_util);
+xlabel('type 2 balking rate');
+ylabel('type 1 balking rate');
+zlabel('utility');
+title('Utility for feasible balking rates');
+
+
